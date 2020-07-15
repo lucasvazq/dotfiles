c7ebfd564732c7fcf97557592e50b652f81b7349
lucasvazq <lucas5zvazquez@gmail.com>
2020-07-14 21:07:24 -0300

    fix: improve files order

--- setup.sh
+++ setup.sh
@@ -136,7 +136,7 @@ fc-cache ~/.local/share/fonts
 # Paste dotfiles and clean directory
 mv ./* ~/
 cd ..
-trash dotfiles .github .git CODE_OF_CONDUCT.md CONTRIBUTING.md LICENCE README.md docs screenshot.png SECURITY.md setup.sh
+trash dotfiles .git .github CODE_OF_CONDUCT.md CONTRIBUTING.md LICENCE README.md docs screenshot.png SECURITY.md setup.sh
 
 # Give permissions to commands
 chmod 711 ~/.local/bin/change-background
